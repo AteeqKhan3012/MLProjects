e6086f71181343968a5a2b6378d4c1dfd698393d
Ateeq Khan <137079156+AteeqKhan3012@users.noreply.github.com>
2024-10-16 15:46:21 +0530

    Delete stringst.py

--- stringst.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("String Example")
-
-# String input
-string_value = st.text_input("Enter a string:", value="Hello, Streamlit!")
-st.text_input("hello")
-# Display the string value
-st.write(f"The string you entered is: {string_value}")
