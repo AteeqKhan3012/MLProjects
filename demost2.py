1eafcb5a32a43dbbc84a9ee70f62727cd76522ae
Ateeq Khan <137079156+AteeqKhan3012@users.noreply.github.com>
2024-10-16 15:53:34 +0530

    Delete demost2.py

--- demost2.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-option = st.selectbox(
-"How would you like to be contacted?",
-("Email", "Home phone", "Mobile phone"),
-placeholder="Select contact method...")
-st.write("You selected:", option)
