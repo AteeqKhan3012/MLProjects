142478b9d2b77e7136f62a613ba78d2212c59e86
Ateeq Khan <137079156+AteeqKhan3012@users.noreply.github.com>
2024-10-16 15:52:50 +0530

    Delete checkst.py

--- checkst.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Boolean Example")
-
-# Boolean value
-is_active = st.checkbox("Is active?", value=True)
-st.checkbox("hello")
-# Display the bo"olean value
-st.write(f"Is active: {is_active}")
